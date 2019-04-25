dbea4ef4bb11a8dbc64364ef675159c470e79c61
Yidi Xu <31810198+Yidi0221@users.noreply.github.com>
2019-04-25 13:14:47 -0400

    HTML1

---
+++ files/home/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L],M,_(N,O,P,Q,R,S,T,_(),U,_(V,W,X,Y,Z,_(ba,bb,bc,bd),be,null,bf,Y,bg,Y,bh,bi,bj,null,bk,bl,bm,bn,bo,bp,bq,bl),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="home.html",d="generationDate",e=new Date(1555808119391.99),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="loginvalue",o="loginornot",p="Q1",q="Q2",r="Total_value",s="rusty_chicken",t="bbq",u="roast",v="recipe_title",w="recipe_description",x="recipe_ingredient",y="recipe_direction",z="recipe_exists",A="Q3",B="Q4",C="Q5",D="A1",E="A2",F="A3",G="A4",H="A5",I="Cart_roast",J="follow_exists",K="username",L="password",M="page",N="packageId",O="a6ab4a38095f4059bfa6baa104a14696",P="type",Q="Axure:Page",R="name",S="Home",T="notes",U="style",V="baseStyle",W="627587b6038d43cca051c114ac41ad32",X="pageAlignment",Y="near",Z="fill",ba="fillType",bb="solid",bc="color",bd=0xFFFFFFFF,be="image",bf="imageHorizontalAlignment",bg="imageVerticalAlignment",bh="imageRepeat",bi="auto",bj="favicon",bk="sketchFactor",bl="0",bm="colorStyle",bn="appliedColor",bo="fontName",bp="Applied Font",bq="borderWidth",br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
